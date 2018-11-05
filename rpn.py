e2f8025df8009d4c6cea14f9de996d7af28e7151
Michael Staunton <mstaunto@umich.edu>
2018-11-05 13:34:10 -0500

    Added Debug Mode to rpn.py

--- rpn.py
+++ rpn.py
@@ -2,6 +2,8 @@
 
 import operator
 import readline
+import pdb
+import sys
 
 
 operators = {
@@ -13,8 +15,10 @@ operators = {
     '%': operator.mod
 }
 
-def calculate(myarg):
+def calculate(myarg, mode):
     stack = list()
+    if mode == 1: # debug mode
+        pdb.set_trace()
     for token in myarg.split():
         try:
             token = int(token)
@@ -25,14 +29,19 @@ def calculate(myarg):
             arg1 = stack.pop()
             result = function(arg1, arg2)
             stack.append(result)
-        print(stack)
     if len(stack) != 1:
         raise TypeError("Too many parameters")
     return stack.pop()
 
 def main():
+    debug_mode = 0
+    if len(sys.argv) == 2:
+        if sys.argv[1] == '-debug':
+            debug_mode = 1
+        else:
+            raise TypeError("Invalid Option")
     while True:
-        result = calculate(input("rpn calc> "))
+        result = calculate(input("rpn calc> "), debug_mode)
         print("Result: ", result)
 
 if __name__ == '__main__':
